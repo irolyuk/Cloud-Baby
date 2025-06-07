102e561b711896a240abddb0f4ae2f8837c9d9cb
Ivan Roliuk <63609486+irolyuk@users.noreply.github.com>
2025-06-07 10:16:36 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request
 from flask_socketio import SocketIO, send
 from flask_cors import CORS
@@ -6,14 +7,10 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on('connect')
-def handle_connect():
-    print(f"Користувач підключився: {request.sid}")
-
 @socketio.on('message')
 def handle_message(msg):
     user = "Baby" if request.sid == list(socketio.server.eio.sockets.keys())[0] else "Pink Cloud"
-    send({"user": user, "text": msg}, broadcast=True)
+    send(json.dumps({"user": user, "text": msg}), broadcast=True)
 
 @app.route('/')
 def index():
