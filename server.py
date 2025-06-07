960e5370673c7129b1eb3a4844ec052fc0aa01aa
Ivan Roliuk <63609486+irolyuk@users.noreply.github.com>
2025-06-07 10:05:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
-import eventlet
-import eventlet.wsgi
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO, send
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('connect')
+def handle_connect():
+    print(f"Користувач підключився: {request.sid}")
 
 @socketio.on('message')
 def handle_message(msg):
-    send(msg, broadcast=True)
+    user = "Baby" if request.sid == list(socketio.server.eio.sockets.keys())[0] else "Pink Cloud"
+    send({"user": user, "text": msg}, broadcast=True)
 
 @app.route('/')
 def index():
@@ -19,5 +22,4 @@ def index():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
-
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', port)), app)
+    socketio.run(app, host="0.0.0.0", port=port)
