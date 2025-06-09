e69626d73e8031631593f46f67667f8a981ac0c1
Ivan Roliuk <irolyuk@gmail.com>
2025-06-09 11:32:56 +0300

    +
    +

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ current_global_track = None # Зберігає поточний глобальн
 
 # --- Тамагочі ---
 tamagotchi_timer = None
-TICK_INTERVAL = 20 # Тимчасово кожні 20 секунд для тестування
+TICK_INTERVAL = 2 # Тимчасово кожні 2 секунд для тестування
 
 DEFAULT_TAMAGOTCHI_STATE = {
     "name": "Хмаринка",
